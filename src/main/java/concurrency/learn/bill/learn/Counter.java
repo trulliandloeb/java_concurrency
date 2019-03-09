40927a0c9dbe46976bf53e825ac9172241b700e5
trulliandloeb <trulliandloeb@hotmail.com>
2019-03-09 21:10:54 +0800

    ReentrantLock

--- src/main/java/concurrency/learn/bill/learn/Counter.java
+++ src/main/java/concurrency/learn/bill/learn/Counter.java
@@ -1,15 +1,22 @@
 package concurrency.learn.bill.learn;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
 	private int count = 0;
+
+	private ReentrantLock lock = new ReentrantLock();
 
 	public int getCount() {
 		return count;
 	}
 
 	public void increment() {
-		synchronized (this) {
+		lock.lock();
+		try {
 			++count;
+		} finally {
+			lock.unlock();
 		}
 	}
 }
--- src/test/java/concurrency/learn/bill/learn/CounterTest.java
+++ src/test/java/concurrency/learn/bill/learn/CounterTest.java
@@ -1,18 +1,16 @@
 package concurrency.learn.bill.learn;
 
 import static concurrency.learn.bill.learn.ConcurrentUtils.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 
-import concurrency.learn.bill.learn.Counter;
-
-public class CounterTest
-
-{
+public class CounterTest {
 	@Test
 	public void incrementWith4Threads() {
 		Counter counter = new Counter();
@@ -23,5 +21,33 @@ public class CounterTest
 		stop(executor);
 
 		System.out.println(counter.getCount());
+
+		assertTrue(true);
+	}
+
+	@Test
+	public void reentrantLockDemo() {
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		ReentrantLock lock = new ReentrantLock();
+
+		executor.submit(() -> {
+			lock.lock();
+			try {
+				sleep(2);
+			} finally {
+				lock.unlock();
+			}
+		});
+
+		executor.submit(() -> {
+			System.out.println("isLocked() : " + lock.isLocked());
+			System.out.println("isHeldByCurrentThread(): " + lock.isHeldByCurrentThread());
+			boolean locked = lock.tryLock();
+			System.out.println("tryLock(): " + locked);
+		});
+
+		stop(executor);
+
+		assertTrue(true);
 	}
 }
